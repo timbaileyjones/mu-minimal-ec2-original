6d8b2dc87a28193fa60121670cb43747e18d30bc
Tim Bailey-Jones <timothy.baileyjones@stelligent.com>
2018-02-09 10:53:35 -0500

    show $PWD and files in immediate directory (trying to debug deploy failure)

--- codedeploy/start_server.sh
+++ codedeploy/start_server.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+echo ==================
+echo starting $0 in `pwd`
+echo files here are: 
+find . -type f -print
+echo ==================
 export PATH=/usr/local/bin:$PATH
 export FLASK_APP=hello.py 
 flask run &
