822619dc509029435dbfb53f3f8c98572a506642
Tim Bailey-Jones <timothy.baileyjones@stelligent.com>
2018-02-09 18:51:45 -0500

    still debugging

--- codedeploy/start_server.sh
+++ codedeploy/start_server.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 echo ==================
 echo starting $0 in `pwd`
 cd `dirname $0`/..
@@ -6,13 +6,20 @@ echo now in $PWD
 echo ==================
 export PATH=/usr/local/bin:$PATH
 export FLASK_APP=hello.py 
+mkdir /tmp/flask
+cp -av ./* /tmp/flask
+
+if [ -f /tmp/flask.pid ]
+then
+  kill -15 `cat /tmp/flask.pid`
+fi
 #
 #  added a little voodoo from the bottom of this page:
 #     https://docs.aws.amazon.com/codedeploy/latest/userguide/troubleshooting-deployments.html#troubleshooting-long-running-processes
 #
-cd /home/ec2-user
-nohup flask run -p 8080 > /dev/null 2> /dev/null < /dev/null &
+flask run -p 8080 > /dev/null 2> /dev/null < /dev/null &
 
 echo $! > /tmp/flask.pid
+sleep 3
 
 exit 0
