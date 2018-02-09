f3ad51e799e3f925c7d9450fa621f73d1a8e39b6
Tim Bailey-Jones <timothy.baileyjones@stelligent.com>
2018-02-09 16:44:32 -0500

    apparently we have to take care stdin/stdout/stderr explictitly or CodeDeploy will HANG

--- codedeploy/start_server.sh
+++ codedeploy/start_server.sh
@@ -6,7 +6,11 @@ echo now in $PWD
 echo ==================
 export PATH=/usr/local/bin:$PATH
 export FLASK_APP=hello.py 
-nohup flask run &
+#
+#  added a little voodoo from the bottom of this page:
+#     https://docs.aws.amazon.com/codedeploy/latest/userguide/troubleshooting-deployments.html#troubleshooting-long-running-processes
+#
+nohup flask run > /dev/null 2> /dev/null < /dev/null &
 
 echo $! > /tmp/flask.pid
 
