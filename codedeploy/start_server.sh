dc51fb0fa09565c4a8c85bdecab17a7f4ed4c491
Tim Bailey-Jones <timothy.baileyjones@stelligent.com>
2018-02-09 16:07:44 -0500

    trying to diagnose why codedeploy is hanging (maybe it's the find command)

--- codedeploy/start_server.sh
+++ codedeploy/start_server.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 echo ==================
 echo starting $0 in `pwd`
-cd `dirname $0` 
-
+cd `dirname $0`/..
 echo now in $PWD
-cd ..
-
-echo now in $PWD
-echo files here are: 
-find . -type f -print
 echo ==================
 export PATH=/usr/local/bin:$PATH
 export FLASK_APP=hello.py 
